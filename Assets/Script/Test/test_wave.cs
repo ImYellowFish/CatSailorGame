b7896750129c73366037265faadd422b635216f4
ImYellowFish <1162793975@qq.com>
2018-11-08 19:28:15 +0800

    init commit

---
+++ Assets/Script/Test/test_wave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test_wave : MonoBehaviour {
+    public float moveSpeed;
+    public float moveDist;
+    private float originalX;
+
+	// Use this for initialization
+	void Start () {
+        originalX = transform.position.x;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        float currentX = transform.position.x;
+        if(Mathf.Abs(currentX - originalX) >= moveDist)
+        {
+            var pos = transform.position;
+            pos.x = originalX;
+            transform.position = pos;
+        }
+	}
+}
